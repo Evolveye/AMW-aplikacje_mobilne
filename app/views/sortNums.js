43daf6549cd10de42b8d15a81c451b0b07816a83
Evolveye <stolarski.paw@gmail.com>
2021-04-20 17:27:14 +0200

    Done nums filters/sorters

--- app/views/sortNums.js
+++ app/views/sortNums.js
@@ -7,23 +7,45 @@ while (uniqueNums.size < 100) uniqueNums.add( Math.floor( Math.random() * 1000 )
 
 export default () => {
   const [ sorter, setSorter ] = useState()
+  const [ filter, setFilter ] = useState()
 
   let numsTexts = Array.from( uniqueNums, (num, i) => ({ num, key:i }) )
 
   if (sorter) numsTexts.sort( sorter )
+  if (filter) numsTexts = numsTexts.filter( filter )
 
   return (
-    <View>
-      <View>
-        <View><Button title="Sortuj rosnąco" onPress={() => setSorter( () => (a, b) => a.num - b.num )} /></View>
-        <View><Button title="Sortuj malejąco" onPress={() => setSorter( () => (a, b) => b.num - a.num )} /></View>
+    <View style={styles.screen}>
+      <View style={styles.buttons}>
+        <View style={styles.sorters}>
+          {
+            [
+              { title:`Sortuj rosnąco`, sorter:(a, b) => a.num - b.num },
+              { title:`Sortuj malejąco`, sorter:(a, b) => b.num - a.num },
+            ].map( ({ title, sorter }) => (
+              <View key={title} style={styles.button}><Button title={title} onPress={() => setSorter( () => sorter )} /></View>
+            ) )
+          }
+        </View>
+
+        <View style={styles.filters}>
+          {
+            [
+              { title:`Wyczyść filtry`, filter:null },
+              { title:`Tylko parzyste`, filter:a => a.num % 2 },
+              { title:`Parzyste i podzielne przez 3`, filter:a => (a.num % 2) && (a.num % 3 == 0) },
+            ].map( ({ title, filter }) => (
+              <View key={title} style={styles.button}><Button title={title} onPress={() => setFilter( () => filter )} /></View>
+            ) )
+          }
+        </View>
       </View>
 
-      <ScrollView contentContainerStyle={styles.screen}>
+      <ScrollView contentContainerStyle={styles.nums}>
         {numsTexts.map( ({ num, key }) => <Text key={key} style={styles.num}>{num}</Text> )}
       </ScrollView>
 
-      <View>
+      <View style={styles.right}>
 
       </View>
     </View>
@@ -33,9 +55,25 @@ export default () => {
 /** @type {Object<string,React.CSSProperties>} */
 const styles = StyleSheet.create({
   screen: {
+    flexDirection: `row`,
+  },
+  buttons: {
+    width: 100,
+    marginRight: 100,
+  },
+  sorters: {
+    width: 100,
+    margin: 20,
+  },
+  filters: {
+    width: 100,
+    margin: 20,
+  },
+  nums: {
     flex: 1,
-    alignItems: `center`,
-    justifyContent: `center`,
+  },
+  button: {
+    margin: 5,
   },
   num: {
     fontFamily: `monospace`,
